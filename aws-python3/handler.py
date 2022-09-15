35308074f2a0b470c554352a8df68bc55f3a9bce
Gabriele Cavigiolo <gabriele.cavigiolo@its-ictpiemonte.it>
2022-09-15 15:46:52 +0200

    Message modificato

--- aws-python3/handler.py
+++ aws-python3/handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "Go Serverless on ITA v1.0! Your function executed successfully!",
         "input": event
     }
 
