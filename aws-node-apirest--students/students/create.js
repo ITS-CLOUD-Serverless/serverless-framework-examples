e3d1fe3ca3beae56e10ba67cd3966a899db20210
Gabriele Cavigiolo <gabri.cavi@gmail.com>
2022-10-20 10:17:35 +0200

    added rest api

---
+++ aws-node-apirest--students/students/create.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.firstName !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the Student.',
+    });
+    return;
+  }
+
+  const { firstName, lastName, course } = data;
+
+  const params = {
+    TableName: process.env.STUDENTS_TABLE,
+    Item: {
+      id: uuid.v1(),
+      firstName,
+      lastname,
+      course,
+      voteAvg : 6,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the Student.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
